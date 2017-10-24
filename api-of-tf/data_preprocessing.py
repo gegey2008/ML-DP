40634cae2e54374657b222b45df406a66eb2dc8f
gegey2008 <milkyang2008@126.com>
2017-10-24 08:04:05 +0800

    add some func

--- api-of-tf/data_preprocessing.py
+++ api-of-tf/data_preprocessing.py
@@ -390,6 +390,96 @@ class ImagePreprocessing(DataPreprocessing):
             new_batch.append(batch[i][nh: nh + shape[0], nw: nw + shape[1]])
         return new_batch
 
+    # ----------------------------------------------
+    # Preprocessing Methods (Overwritten from Base)
+    # ----------------------------------------------
+
+    def add_samplewise_zero_center(self, per_channel=False):
+        """ add_samplewise_zero_center.
+
+        Zero center each sample by subtracting it by its mean.
+
+        Arguments:
+            per_channel: `bool`. If True, apply per channel mean.
+
+        Returns:
+            Nothing.
+
+        """
+        self.methods.append(self._samplewise_zero_center)
+        self.args.append([per_channel])
+
+    def add_samplewise_stdnorm(self, per_channel=False):
+        """ add_samplewise_stdnorm.
+
+        Scale each sample with its standard deviation.
+
+        Arguments:
+            per_channel: `bool`. If True, apply per channel std.
+
+        Returns:
+            Nothing.
+
+        """
+        self.methods.append(self._samplewise_stdnorm)
+        self.args.append([per_channel])
+
+    def add_featurewise_zero_center(self, mean=None, per_channel=False):
+        """ add_samplewise_zero_center.
+
+        Zero center every sample with specified mean. If not specified,
+        the mean is evaluated over all samples.
+
+        Arguments:
+            mean: `float`(optional). Provides a custom mean. If none
+            provided, it will be automatically caluclated based on
+            the training dataset. Default: None.
+            per_channel: `bool`. If True, compute mean per color channel.
+
+        Returns:
+            Nothing.
+
+        """
+        self.global_mean.is_required = True
+        self.global_mean.value = mean
+        if per_channel:
+            self.global_mean_pc = True
+        self.methods.append(self._featurewise_zero_center)
+        self.args.append(None)
+
+    def add_featurewise_stdnorm(self, std=None, per_channel=False):
+        """ add_samplewise_stdnorm.
+
+        Scale each sample by specified standard deviation. If no std specified,
+        std is evaluated over all samples data.
+
+        Arguments:
+            std: `float`(optional). Provides a custom standard derivation. If none
+            provided, it will be automatically caluclated based on
+            the training dataset. Default: None.
+            per_channel: `bool`. If True, compute std per color channel.
+
+        Returns:
+            Nothing.
+
+        """
+        self.global_std.is_required = True
+        self.global_std.value = std
+        if per_channel:
+            self.global_std_pc = True
+        self.methods.append(self._featurewise_stdnorm)
+        self.args.append(None)
+
+    # -------------------------------------------------------------
+    # Calulation with Persistent Parameters (Overwritten from Base)
+    # -------------------------------------------------------------
+
+
+
+
+
+
+
 
 
 
