0a37ea60f1323037cda29149cb55992ecf65fbe8
gegey2008 <milkyang2008@126.com>
2017-11-13 22:50:03 +0800

    fix some file

---
+++ api-of-tf/tflearn/datasets/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*
+###########################
+#File Name: __init__.py
+#Author: gegey2008
+#Mail: milkyang2008@126.com
+#Created Time: 2017-11-13 19:43:07
+############################
+
+from . import cifar10
+from . import imdb
+from . import mnist
+from . import oxflower17
+from . import titanic
+
+
+
---
+++ api-of-tf/tflearn/datasets/imdb.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*
+###########################
+#File Name: imdb.py
+#Author: gegey2008
+#Mail: milkyang2008@126.com
+#Created Time: 2017-11-13 21:04:14
+############################
+
