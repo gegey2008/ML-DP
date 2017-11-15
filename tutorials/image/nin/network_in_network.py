c6582e4871300901df559a27d99e39ae1f607536
gegey2008 <milkyang2008@126.com>
2017-11-16 07:57:15 +0800

    add func for NIN

--- tutorials/image/nin/network_in_network.py
+++ tutorials/image/nin/network_in_network.py
@@ -7,7 +7,7 @@
 #Created Time: 2017-11-15 22:55:13
 ############################
 
-from __future__ import absolute_import, division, print_function
+from __future_ _ import absolute_import, division, print_function
 
 import argparse   #解析命令行参数和选项
 import os
@@ -37,7 +37,42 @@ parser.add_argument('--use_fp16', type=bool, default=False,
 #调用方法解析
 FLAGS = parser.parse_args()
 
+#Global constans describing the CIFAR-10 data set.
+IMAGE_SIZE = cifar10_input.IMAGE_SIZE
+NUM_CLASSES = cifar10_input.NUM_CLASSES
+NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = cifar10_input.NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN
+NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = cifar10_input.NUM_EXAMPLES_PER_EPOCH_FOR_EVAL
 
+#Constants describing the training process.
+MOVING_AVERAGE_DECAY = 0.9999     # the decay to use for the moving average.
+NUM_EPOCHS_PER_DECAY = 350.0      # Epochs after which learning rate decays.
+LEARNING_RATE_DECAY_FACTOR = 0.1  # Learning rate decay factor.
+INITIAL_LEARNING_RATE = 0.1       # Initial learning rate.
+
+# If a model is trained with multiple GPUs, prefix all Op names with tower_name
+# to differetiate the operations. Note that this prefix is removed from the
+# names of the summaries when visualizing a model.
+TOWER_NAME = 'tower'
+
+DATA_URL = 'http://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz'
+
+
+def _activation_summary(x):
+    """Helper to create summaries for activations.
+
+    Creates a summary that provides a histogram of activations.
+    Creates a summary that measures the sparsity of activations.
+
+    Args:
+        x: Tensor
+    Returns:
+        nothing
+    """
+    # Remove 'tower_[0-9]/' from the name in case this a multi-GPU training
+    # session. This helps the clarity of presentation on tensorboard.
+    tensor_name = re.sub('%s_[0-9]*/' % TOWER_NAME, '', x.op.name)
+    tf.summar.histogram(tensor_name + '/activations', x)
+    tf.summary.scalar(tensor_name + '/sparsity', tf.nn.zero_fraction(x))
 
 
 
